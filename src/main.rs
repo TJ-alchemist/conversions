aa274f27557d8e2543658111cf87864205852483
Tejas <tejasvijay09@gmail.com>
2022-04-15 02:10:28 +0530

    modify: applied formatting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-
 mod dec_to_hex;
 use dec_to_hex::dec_to_hex;
 
 fn main() {
-	// Converting decimal number to hexadecimal number
-	println!("{}", dec_to_hex(5386));
+    // Converting decimal number to hexadecimal number
+    println!("{}", dec_to_hex(5386));
 }
